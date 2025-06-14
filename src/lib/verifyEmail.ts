8e14f442736e348abc2c55a2faf287217ff0283a
Ezequiel <ezequielezequiel9@gmail.com>
2025-06-14 20:42:23 -0300

    apiKey change

--- src/lib/verifyEmail.ts
+++ src/lib/verifyEmail.ts
@@ -1,7 +1,6 @@
 //verifico el email
 export async function verifyEmailWithHunter(email) {
-  const apiKey = process.env.HUNTER_API_KEY;
-  const url = `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`;
+  const url = `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${process.env.HUNTER_API_KEY}`;
 
   const response = await fetch(url);
   const info = await response.json();
