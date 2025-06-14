43933d92c6e8ff46e80291c9452f28f215a073ad
Ezequiel <ezequielezequiel9@gmail.com>
2025-06-14 20:02:27 -0300

    edit route product [id]

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -2,7 +2,7 @@ import { getProductByIdController } from "controllers/products";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, context: any) {
-  const { id } = await context.params;
+  const { id } = context.params;
   try {
     const result = await getProductByIdController(id);
     if (!result.success) {
